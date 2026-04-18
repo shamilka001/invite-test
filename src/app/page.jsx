591085d3fd5aba947c1eeb4410c650a8798b59da
Shamilka Peiris <114128164+shamilka001@users.noreply.github.com>
2026-04-19 00:15:35 +0530

    rsvp

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -85,13 +85,14 @@ export default function Invitation() {
 
       <SwiperSlide>
         <div className="flex flex-col items-center justify-center h-screen text-center bg-transparent">
-          <h1 className=" text-yellow-800 text-[40px] font-bold">Holy Mass</h1>
+          <h1 className="text-yellow-800 text-[40px] font-bold">Holy Mass</h1>
           
           <iframe 
           src="https://www.google.com/maps?q=Hilton+Colombo+Residences&output=embed"
           loading="lazy"className="mt-4 w-50 h-80 rounded-t-full border border-solid border-blue-800 shadow-md">
 
-</iframe><p className="text-[12px] mt-2">Hilton Colombo Residences</p>
+</iframe><p className="text-[15px] mt-6">Church Name Here</p>
+<p className="text-[20px] mt-6">Starting @ 10:00 AM</p>
         </div>
       </SwiperSlide>
 
@@ -103,13 +104,14 @@ export default function Invitation() {
 src="https://www.google.com/maps?q=Hilton+Colombo+Residences&output=embed"
 loading="lazy"className="mt-4 w-50 h-80 rounded-t-full border border-solid border-blue-800">
 
-</iframe><p className="text-[12px] mt-2">Hilton Colombo Residences</p>
+</iframe><p className="text-[15px] mt-6">Hilton Colombo Residences</p>
+<p className="text-[20px] mt-6">Starting @ 4:00 PM</p>
         </div>
       </SwiperSlide>
 
       <SwiperSlide>
         <div className="flex flex-col items-center justify-center h-screen text-center bg-transparent">
-          <h1 className=" text-yellow-800 text-[40px] font-bold">Slide 3</h1>
+          <h1 className=" text-yellow-800 text-[40px] font-bold">RSVP</h1>
           <form className="flex flex-col gap-3 mt-4 w-64">
   <input className="p-2 rounded bg-white/80 text-black" type="text" placeholder="Name" required />
   
@@ -125,20 +127,12 @@ loading="lazy"className="mt-4 w-50 h-80 rounded-t-full border border-solid borde
 
   <textarea className="p-2 rounded bg-white/80 text-black" placeholder="Message"></textarea>
 
-  <button className=" bg-yellow-600 text-white py-2 rounded-full w-32 align-middle self-center mt-4" type="submit">
+  <button className="border border-solid border-blue-800 text-yellow-600 py-2 rounded-full w-32 align-middle self-center mt-4" type="submit">
     Submit RSVP
   </button>
 </form>
           
           
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide>
-        <div className="flex flex-col items-center justify-center h-screen text-center bg-transparent">
-          <h1 className="text-[40px] font-bold">Slide 4</h1>
-          <p className="text-[12px] mt-2">Second section</p>
-          <img src="/your-image.jpg" className="mt-4 w-40" />
         </div>
       </SwiperSlide>
     
